dfd5138bc45d497aeeb6d09fa0d6e4b1c7afd346
johnklima <klima.de.sa@gmail.com>
2025-10-02 14:37:42 +0200

    add first aniimations and RPS LUT

---
+++ Assets/Scripts/RoclPaperScissors.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class RoclPaperScissors : MonoBehaviour
+{
+
+    //RPS LUT
+
+                //rock      //paper     //scissors
+    //rock      0            1            2
+
+    //paper     2            0            1
+
+    //scissors  1            2            0
+
+
+    // 0 draw
+    // 1 loss
+    // 2 win
+
+    int[,] resultTable = new int[3, 3];
+
+    int ROCK = 0;
+    int PAPER = 1;
+    int SCISSORS = 2;
+
+    int DRAW = 0;
+    int LOSS = 1;
+    int WIN = 2;
+
+    string[] play = { "Rock", "Paper", "Scissors" };
+    string[] result = { "Draw", "Loss", "Win" };
+
+    public bool playIt = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        resultTable[ROCK, ROCK] = DRAW;     //is tie
+        resultTable[ROCK, PAPER] = LOSS;    //is loss
+        resultTable[ROCK, SCISSORS] = WIN;  //is win
+
+        resultTable[PAPER, ROCK] = WIN;
+        resultTable[PAPER, PAPER] = DRAW;
+        resultTable[PAPER, SCISSORS] = LOSS;
+
+        resultTable[SCISSORS, ROCK] = LOSS;
+        resultTable[SCISSORS, PAPER] = WIN;
+        resultTable[SCISSORS, SCISSORS] = DRAW;
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playIt)
+        {
+            playIt = false;
+
+            int me = Random.Range(0, 3);
+            int him = Random.Range(0, 3);
+
+            int r = resultTable[me, him];
+
+            Debug.Log("I played " + play[me] + " he played " + play[him]);
+            Debug.Log("I " + result[r]);
+
+        }
+    }
+}
